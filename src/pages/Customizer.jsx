bc04afa9f2942808e3cbb915b63012d89879e8b7
Bogdan Grabinskij <admin@example.com>
2024-02-02 12:52:27 +0100

    Replace ColorPicker

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -14,13 +14,14 @@ const Customizer = () => {
                 <div className="
                 absolute
                 z-10
-                lg:top-1/2 lg:left-0 lg:ml-28 lg:mt-32 lg:-translate-y-1/2 lg:-mt-12
+                 lg:left-0 lg:ml-28 lg:mt-32 lg:-translate-y-1/2 lg:mt-40
                 transform
                 -translate-x-1/2
                 left-1/2
-                top-0"
+                top-0
+                -mt-8"
                 >
-                    <div className=" bg-stone-500 flex items-center">
+                    <div>
                         <ColorPicker />
                     </div>
                 </div>
